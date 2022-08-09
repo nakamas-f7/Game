90d61ce6f3c6b9e12a3344cfcd48a5e16c83caaf
Carlos(escola01) <ceinvestimentosllc@email.com>
2022-08-09 13:43:46 -0300

    Resolvi bugs resolvi o pulo e vazamentos de margem

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
-
-/*============ Variaveis ============*/ 
+/*============ Variaveis constantes de ligação html============*/ 
 
 const body = document.querySelector('body')
-
 const pista = document.getElementById('gif')
-
 const gif = document.getElementById('gif') 
-
 const banco = document.getElementById('banco')
 
+/*============ Variaveis controladoras ============*/ 
+
 let valorX = 0
-let altura = 0
-let controladorDirecao = 'null'
+let controladorDirecao = 'Direita'
 let controladorPulo = 0
+
+/*============ Variaveis botões ============*/ 
+
+let KeyA = true
+let KeyD = true
+let KeySpace = true
 
 /*============ Funções ============*/ 
 
@@ -20,6 +23,7 @@ function EsquerdaDireita(){
     body.onkeydown = function(event){
         console.log(event.code)
         if(event.code === 'KeyD'){
+            KeyD = false
             if(valorX < 90 ){
                 if((valorX + valorX) >= 0){
                     valorX += 1.5
@@ -27,6 +31,7 @@ function EsquerdaDireita(){
                 }
             }
         }else if(event.code === 'KeyA'){
+            KeyA = false
             if(valorX >= 1.5 ){
                 if((valorX + valorX) >= 1){
                     valorX -= 1.5
@@ -34,54 +39,81 @@ function EsquerdaDireita(){
                 }                
             }
         }else if(event.code === 'Space'){
-            if(altura === 0){
-                altura = 20
-                mexer(altura, 'Space')
+            if(KeySpace === true){
+                KeyA = false
+                KeyD = false
+                KeySpace = false
+                pulo()
             }
         }
     }
 }
-
-
 function pulo(){
-    if(controladorDirecao === 'Esquerda'){
-        if(valorX - 10 >= 0 ){
-            valorX -= 10 
-            gif.style.marginLeft = valorX + '%'
-        }else if(valorX - 10 < 0){
-            valorX = 0
-            gif.style.marginLeft = 0
+    if(controladorDirecao === 'Direita'){
+        if(valorX + 10 <= 90){
+            document.documentElement.style.setProperty('--pulo-atras', (valorX + 10) + '%');
+            gif.style.animation = 'puloatras 1s linear'
+            setTimeout(function(){
+                valorX += 10
+                gif.style.marginLeft = valorX + '%'
+                gif.style.animation = 'none'
+                KeySpace = true
+                KeyA = true
+                KeyD = true
+            }, 1000)
+        }else if(valorX + 10 > 90){
+            document.documentElement.style.setProperty('--pulo-atras', 90 + '%');
+            gif.style.animation = 'puloatras 1s linear'
+            setTimeout(function(){
+                valorX = 90
+                gif.style.marginLeft = valorX + '%'
+                gif.style.animation = 'none'
+                KeySpace = true
+                KeyA = true
+                KeyD = true
+            }, 1000)
         }
-    }else if(controladorDirecao === 'Direita'){
-        if(valorX + 10 <= 100){
-            valorX += 10 
-            gif.style.marginLeft = valorX + '%'
-        }else if(valorX + 10 > 100){
-            valorX = 100
-            gif.style.marginLeft = valorX + '%'
+    }else if(controladorDirecao === 'Esquerda'){
+        if(valorX - 10 >= 0){
+            document.documentElement.style.setProperty('--pulo-atras', (valorX - 10) + '%');
+            gif.style.animation = 'puloatras 1s linear'
+            
+            setTimeout(function(){
+                valorX -= 10
+                gif.style.marginLeft = valorX + '%'
+                gif.style.animation = 'none'
+                KeySpace = true
+                KeyA = true
+                KeyD = true
+            }, 1000)
+        }else if(valorX - 10 < 0){
+            document.documentElement.style.setProperty('--pulo-atras', 0 + '%');
+            gif.style.animation = 'puloatras 1s linear'  
+            setTimeout(function(){
+                valorX = 0
+                gif.style.marginLeft = valorX + '%'
+                gif.style.animation = 'none'
+                KeySpace = true
+                KeyA = true
+                KeyD = true
+            }, 1000)            
         }
     }
-    
 }
+
 function mexer(valor, direcao){
     if(direcao === 'KeyD'){
         controladorDirecao = 'Direita'
         gif.style.marginLeft = valor + '%'
+        KeyD = true
     }else if(direcao === 'KeyA'){
         controladorDirecao = 'Esquerda'
         gif.style.marginLeft = valor + '%'
-    }else if(direcao === 'Space'){
-        pulo()
-        gif.style.marginBottom = valor + '%'
-        gif.style.animation = 'gif 1s linear'
-        setTimeout(function(){
-            gif.style.animation = 'none'
-            gif.style.marginBottom = 0
-        }, 1000)
-        altura = 0
+        KeyA = true
     }
 }
 function iniciar(){
     EsquerdaDireita()
 }
+
 iniciar()
